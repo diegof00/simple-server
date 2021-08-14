c2caf3d0dceff55efe11a8383ec93ab38c90a3d1
diegofernando <dfreyes00@gmail.com>
2021-08-13 22:20:52 -0500

    java 11 HttpServer

---
+++ src/co/ddev/examples/Main.java
@@ -0,0 +1,34 @@
+package co.ddev.examples;
+
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            server.createContext("/", getHttpHandler());
+            server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static HttpHandler getHttpHandler() {
+        return exchange -> {
+            String response = "Hello from server";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+            outputStream.close();
+        };
+    }
+}
